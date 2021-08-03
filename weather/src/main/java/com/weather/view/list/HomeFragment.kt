8190cda45021aa748f25bf34d7b81e74a5b3837c
Ali Adnan <aliadnanciit@gmail.com>
2021-08-03 22:25:52 +0400

    add search bar and link with detail search UI

--- weather/src/main/java/com/weather/view/list/HomeFragment.kt
+++ weather/src/main/java/com/weather/view/list/HomeFragment.kt
@@ -1,110 +1,49 @@
 package com.weather.view.list
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.weather.R
-import com.weather.common.DensityConverter
-import com.weather.common.ItemHorizontalSpaceDecoration
-import com.weather.common.ItemVerticalSpaceDecoration
-import com.weather.databinding.ActivityWeatherListBinding
+import com.weather.databinding.FragmentHomeBinding
 import com.weather.model.Weather
-import com.weather.model.server.WeatherStates
-import com.weather.view.detail.WeatherDetailActivity
 import com.weather.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(), WeatherClickListener {
 
     private val viewModel: WeatherViewModel by viewModels()
 
-    private lateinit var binding: ActivityWeatherListBinding
+    private lateinit var binding: FragmentHomeBinding
     private lateinit var adapter : WeatherListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = ActivityWeatherListBinding.inflate(layoutInflater)
-
-        binding.retry.setOnClickListener {
-            loadCampaignsData()
-        }
-        adapter = WeatherListAdapter(this)
-
-        binding.campaignsRecycler.adapter = adapter
-        binding.campaignsRecycler.addItemDecoration(
-            ItemVerticalSpaceDecoration(
-                DensityConverter.toPixel(
-                    resources, resources.getInteger(R.integer.weather_list_item_vertical_spacing)
-                )
-            )
-        )
-        binding.campaignsRecycler.addItemDecoration(
-            ItemHorizontalSpaceDecoration(
-                DensityConverter.toPixel(
-                    resources, resources.getInteger(R.integer.weather_list_item_horizontal_spacing)
-                )
-            )
-        )
+        binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.weatherStateFlow.collect { campaigns ->
-                    processViewState(campaigns)
-                }
-            }
+        initViewSetup()
+    }
+
+
+    private fun initViewSetup() {
+        binding.searchProxy.setOnClickListener {
+            val uri = getString(R.string.deeplink_weather_details).replace("{value}", "true")
+            findNavController().navigate(Uri.parse(uri))
         }
-        loadCampaignsData()
-    }
-
-    private fun processViewState(weatherState: WeatherStates) {
-        binding.loadingIndicator.visibility = View.GONE
-        binding.errorContainer.visibility = View.GONE
-        binding.noContent.visibility = View.GONE
-        binding.campaignsRecycler.visibility = View.GONE
-        when(weatherState) {
-            is WeatherStates.Loading -> {
-                binding.loadingIndicator.visibility = View.VISIBLE
-            }
-            is WeatherStates.NoContent -> {
-                binding.noContent.visibility = View.VISIBLE
-            }
-            is WeatherStates.Success -> {
-                binding.campaignsRecycler.visibility = View.VISIBLE
-                showDate(weatherState.list)
-            }
-            is WeatherStates.Error -> {
-                binding.errorContainer.visibility = View.VISIBLE
-            }
-        }
-    }
-
-    private fun loadCampaignsData() {
-        binding.loadingIndicator.visibility = View.GONE
-        binding.errorContainer.visibility = View.GONE
-//        viewModel.fetchCampaigns()
-        viewModel.fetchWeekWeather()
-    }
-
-    private fun showDate(list: List<Weather>) {
-        adapter.submitList(list)
     }
 
     override fun onclick(weather: Weather) {
-        WeatherDetailActivity.startDetailActivity(requireActivity(), weather)
+        TODO("Not yet implemented")
     }
 }
